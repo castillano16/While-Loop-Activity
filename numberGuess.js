bd4c6ceb57b94899082e82d50b20d5c2cbb452e2
castillano16 <marbelencastillano3@gmail.com>
2025-03-20 20:47:36 +0800

    Update numberGuess.js

--- numberGuess.js
+++ numberGuess.js
@@ -1,8 +1,7 @@
-const correctNumber = Math.floor(Math.random() * 10) + 1; // Generate a random number between 1 and 10
 let guess;
 
 while (true) {
-    guess = parseInt(prompt("Guess a number between 1 and 10:"), 10); // Prompt user input
+    guess =prompt("Guess a number between 1 and 10:"), 10); // Prompt user input
 
     if (isNaN(guess) || guess < 1 || guess > 10) {
         console.log("Invalid input. Please enter a number between 1 and 10.");
